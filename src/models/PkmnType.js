8dddbf83fca166b60e012123e3d323ef69d4b019
yanischkouchi <yanischkouchi@gmail.com>
2025-02-13 17:21:43 +0100

    installation

---
+++ src/models/PkmnType.js
@@ -0,0 +1,5 @@
+const PkmnType = [
+    "Normal", "Feu", "Eau", "Plante", "Électrik", "Glace", 
+    "Combat", "Poison", "Sol", "Vol", "Psy", "Insecte", 
+    "Roche", "Spectre", "Dragon", "Ténèbres", "Acier", "Fée"
+];
---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const apiRouteur = require('./routes/api')
+const wss = require('./websocketserver')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// connexion à l'API
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/data')
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+app.use('/api', apiRouteur);
